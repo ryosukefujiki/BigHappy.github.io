3165d857e56ef0edc1bf0dfb982e150f68984144
daifuku <fis10sai@i.softbank.jp>
2017-06-08 09:19:33 +0900

    first commit

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+jQuery(function(){
+    jQuery('#scroll').click(function(){
+        var speed = 400;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top + 700;
+        jQuery("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
+});
