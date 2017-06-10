ffd3f42cb5f12e658bcd190197ccd960bae1de00
daifuku <fis10sai@i.softbank.jp>
2017-06-10 20:24:48 +0900

    change scroll height in scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,15 @@
 jQuery(function(){
     jQuery('#scroll').click(function(){
+        var addHeight = 0;
+        if(window.parent.screen.width >= 768){
+            addHeight = 700;
+        }else{
+            addHeight = 550;
+        }
         var speed = 400;
         var href= $(this).attr("href");
         var target = $(href == "#" || href == "" ? 'html' : href);
-        var position = target.offset().top + 550;
+        var position = target.offset().top + addHeight;
         jQuery("html, body").animate({scrollTop:position}, speed, "swing");
         return false;
     });
