86acfd8a04503ac273b85a6f81b28d1ef7f756c1
daifuku <fis10sai@i.softbank.jp>
2018-09-01 20:20:22 +0900

    SPのタッチイベントをconsole表示

--- js/header_menu.js
+++ js/header_menu.js
@@ -6,8 +6,12 @@ $(document).ready(function() {
 
   $('.swiper-slide')
       .bind( 'touchstart', function(){
+        console.log("タッチしたよ");
         $( this ).addClass( 'hover' );
     }).bind( 'touchend', function(){
+        console.log("手を離した");
         $( this ).removeClass( 'hover' );
+    }).bind( 'touchmove', function(){
+        console.log("動いたよ");
     });
 });
